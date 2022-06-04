d058066a258a19bb5a0273dbb4e8be7c40df2604
Vtl <v71n57@gmail.com>
2022-06-04 10:58:15 +0300

    add

--- vars/runWerf.groovy
+++ vars/runWerf.groovy
@@ -5,12 +5,12 @@ def call(String dockerCreds, String werfargs){
   // второй - имя Jenkins-секрета, где лежат доступы (login, password)
   docker.withRegistry("https://registry.example.com:5000", "${dockerCreds}") {
     sh """#!/bin/bash -el
-          set -o pipefail
+          set -xeo pipefail
           type trdl && source "\$(trdl use werf 1.2 stable)"
           pwd
           id
+          trdl add werf https://tuf.werf.io 1 b7ff6bcbe598e072a86d595a3621924c8612c7e6dc6a82e919abe89707d7e3f468e616b5635630680dd1e98fc362ae5051728406700e6274c5ed1ad92bea52a2
           werf version
-          echo 111111 ${werfargs}
           werf ${werfargs}""".trim()
     }
 }
