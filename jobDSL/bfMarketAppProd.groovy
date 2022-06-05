a2a75ef72d779a5c6075aa9ea227c952fa8904bc
Vtl <v71n57@gmail.com>
2022-06-06 00:57:08 +0300

    add

--- jobDSL/bfMarketAppProd.groovy
+++ jobDSL/bfMarketAppProd.groovy
@@ -1,5 +1,5 @@
+def repo = 'https://github.com/v71n57/bfmarket-app.git'
 pipelineJob('bfmarket-app-prod') {
-  def repo = 'https://github.com/v71n57/bfmarket-app.git'
   description("Pipeline for $repo")
   definition {
     cpsScm {
