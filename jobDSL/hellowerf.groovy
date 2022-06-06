678dca87d077e2e7113eefd1742c1e28033a6979
Vtl <v71n57@gmail.com>
2022-06-07 00:23:36 +0300

    add

--- jobDSL/hellowerf.groovy
+++ jobDSL/hellowerf.groovy
@@ -1,12 +1,14 @@
-pipelineJob('hellowerf') {
-  def repo = 'https://github.com/v71n57/hellowerf.git'
-  description("Pipeline for $repo")
+#!/usr/bin/env groovy
+def repo = "https://github.com/v71n57/hellowerf.git"
+def branchName = "main"
+pipelineJob("hellowerf-${branchName}") {
+  description("Pipeline for $repo branch ${branchName}")
   definition {
     cpsScm {
       scm {
         git {
           remote { url(repo) }
-          branch('main')
+          branch(branchName)
           scriptPath('Jenkinsfile')
         }
       }
