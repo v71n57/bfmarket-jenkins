b80509979fb1979c1548db37b43175f51461dcbf
Vtl <v71n57@gmail.com>
2022-06-07 01:05:33 +0300

    add

--- jobDSL/bfMarketApp.groovy
+++ jobDSL/bfMarketApp.groovy
@@ -1,11 +1,16 @@
 #!/usr/bin/env groovy
 def repo = "https://github.com/v71n57/bfmarket-app.git"
+
 multibranchPipelineJob("bfmarket-app") {
     branchSources {
         branchSource {
             source {
                 git {
                     remote(repo)
+                    traits {
+                        gitBranchDiscovery()
+                        gitTagDiscovery() // if you need tag discovery
+                    }
                 }
             }
             strategy {
@@ -17,12 +22,4 @@ multibranchPipelineJob("bfmarket-app") {
             }
         }
     }
-    configure {
-        def traits = it / sources / data / 'jenkins.branch.BranchSource' / source / traits
-        traits << 'jenkins.plugins.git.traits.BranchDiscoveryTrait' {}
-    }
-    triggers {
-        periodic(2) // Trigger every 2 min.
-    }
-    orphanedItemStrategy { discardOldItems { numToKeep(-1) } }
 }
