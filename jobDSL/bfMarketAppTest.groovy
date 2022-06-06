55cf0ac55f878c882aa48c341b31124c740d8b6a
Vtl <v71n57@gmail.com>
2022-06-06 23:44:35 +0300

    add

--- jobDSL/bfMarketAppTest.groovy
+++ jobDSL/bfMarketAppTest.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 def repo = 'https://github.com/v71n57/bfmarket-app.git'
 def branchName = 'test'
-pipelineJob('bfmarket-app-test') {
+pipelineJob("bfmarket-app-${branchName}") {
   description("Pipeline for $repo branch ${branchName}")
   definition {
     cpsScm {
